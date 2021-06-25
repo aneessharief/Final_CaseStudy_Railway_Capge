87d91e085c7cc6037a643a2bf4f758376bdb3304
aneessharief <82229933+aneessharief@users.noreply.github.com>
2021-06-25 20:51:57 +0530

    Add files via upload

---
+++ ZuulAPIGateway/src/main/java/com/example/ZuulAPIGateway/ZuulApiGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.example.ZuulAPIGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+
+@SpringBootApplication
+@CrossOrigin(origins="localhost:3000")
+@EnableZuulProxy
+public class ZuulApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApiGatewayApplication.class, args);
+	}
+
+}
